28939401a28fec3e56e854de5830b2571144fc41
imruls <imruls95.e@gmail.com>
2026-03-04 18:20:49 -0300

    vercel-analytics-correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://duostudiodigital.com.ar',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+    speedInsights: { enabled: true },
+  }),
   integrations: [sitemap()],
 });
